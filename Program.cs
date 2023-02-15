07b8a7b8ab357185e841d9e18636d02c87ae37d3
Ulinici Pavel <ulinici.pasa@mail.ru>
2023-02-15 12:41:58 +0200

    SecondCommit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program {
+    static void Main(string[] args) {
+        // Объявление и заполнение первоначального массива строк
+        string[] initialArray = { "hello", "2", "world", ":-)"};
+
+        // Создание нового массива строк
+        string[] newArray = new string[initialArray.Length];
+        int count = 0;
+
+        // Проверка каждой строки на длину
+        foreach (string s in initialArray) {
+            if (s.Length <= 3) {
+                newArray[count] = s;
+                count++;
+            }
+        }
+
+        // Вывод нового массива строк
+        for (int i = 0; i < count; i++) {
+            Console.WriteLine(newArray[i]);
+        }
+    }
+}
